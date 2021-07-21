8b1462db2992544f2f26ff78b8d5a31124eb0830
Vivek <Vivek@DESKTOP-2387BPC.bbrouter>
2021-07-21 15:19:34 +0530

    Version 2

---
+++ OOPS/src/superHero.java
@@ -0,0 +1,13 @@
+import java.swing.JOptionPane;
+public class superHero {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		String heroType=JOptionPane.showInputDialog("Enter Typr Hero: ");
+		String heroType=JOptionPane.showInputDialog("Hero Name: ");
+		Hero hero;
+		Class x=Class.forName=
+
+	}
+
+}
